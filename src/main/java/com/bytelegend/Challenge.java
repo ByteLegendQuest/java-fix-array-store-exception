fd6a41b084f05dd7218ba1fd96c250ea3ab0c8a3
gtn1024 <tony.bazhang@gmail.com>
2022-03-16 02:20:16 +0000

    Update at 2022-03-16T02:20:16.819605Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -1,5 +1,7 @@
 package com.bytelegend;
 
+import java.util.Arrays;
+
 public class Challenge {
     public static void main(String[] args) {
         System.out.println(incrementNumbersInCsv("1,2,3"));
@@ -7,10 +9,9 @@ public class Challenge {
     }
 
     public static String incrementNumbersInCsv(String str) {
-        Object[] numbers = str.split(",");
-        for (int i = 0; i < numbers.length; ++i) {
-            numbers[i] = Integer.parseInt(numbers[i].toString()) + 1;
-        }
-        return String.join(",", (String[]) numbers);
+        final String[] strings = Arrays.stream(str.split(","))
+                .map(it -> String.valueOf(Integer.parseInt(it) + 1))
+                .toArray(String[]::new);
+        return String.join(",", strings);
     }
 }
