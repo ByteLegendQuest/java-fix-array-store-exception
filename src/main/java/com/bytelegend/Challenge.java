6967954dc6e6d8b1f4df0725a08c671dda1930c4
wangjie-fourth <wangjie_fourth@163.com>
2022-03-20 10:29:51 +0000

    Update at 2022-03-20T10:29:51.533260Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -1,5 +1,8 @@
 package com.bytelegend;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Challenge {
     public static void main(String[] args) {
         System.out.println(incrementNumbersInCsv("1,2,3"));
@@ -7,10 +10,10 @@ public class Challenge {
     }
 
     public static String incrementNumbersInCsv(String str) {
-        Object[] numbers = str.split(",");
-        for (int i = 0; i < numbers.length; ++i) {
-            numbers[i] = Integer.parseInt(numbers[i].toString()) + 1;
+        List<String> numbers = Arrays.asList(str.split(","));
+        for (int i = 0; i < numbers.size(); ++i) {
+            numbers.set(i, (Integer.parseInt(numbers.get(i)) + 1) + "");
         }
-        return String.join(",", (String[]) numbers);
+        return String.join(",", numbers);
     }
 }
