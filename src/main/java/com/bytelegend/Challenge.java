bddc4920cf1e339aa0585ceb21eb8f6f29e6dcf3
Zuoxq99 <957974737@qq.com>
2022-03-24 08:33:16 +0000

    Update at 2022-03-24T08:33:16.657907Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -6,11 +6,16 @@ public class Challenge {
         System.out.println(incrementNumbersInCsv("-1,999"));
     }
 
+
+
     public static String incrementNumbersInCsv(String str) {
-        Object[] numbers = str.split(",");
+
+        String[] numbers = str.split(",");
         for (int i = 0; i < numbers.length; ++i) {
-            numbers[i] = Integer.parseInt(numbers[i].toString()) + 1;
+
+            numbers[i] = String.valueOf(Integer.parseInt(numbers[i]) + 1);
         }
-        return String.join(",", (String[]) numbers);
-    }
+
+        return String.join(",", numbers);
+     }
 }
