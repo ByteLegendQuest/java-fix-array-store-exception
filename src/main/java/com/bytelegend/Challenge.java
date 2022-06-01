d0380aa5a9726744ac492e56ac14ffdaa1e8888b
s9x <rich@xtinction.org>
2022-06-01 03:22:39 +0000

    Update at 2022-06-01T03:22:39.028501Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -7,10 +7,10 @@ public class Challenge {
     }
 
     public static String incrementNumbersInCsv(String str) {
-        Object[] numbers = str.split(",");
+        String[] numbers = str.split(",");
         for (int i = 0; i < numbers.length; ++i) {
-            numbers[i] = Integer.parseInt(numbers[i].toString()) + 1;
+            numbers[i] = Integer.toString(Integer.parseInt(numbers[i].toString()) + 1);
         }
-        return String.join(",", (String[]) numbers);
+        return String.join(",", numbers);
     }
 }
