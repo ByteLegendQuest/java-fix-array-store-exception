971f4aff5b18a594a3cbe4b2eda2d5fda41e3248
karolk75 <karolk410410@gmail.com>
2022-06-25 21:36:29 +0000

    Update at 2022-06-25T21:36:29.939040Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -1,5 +1,8 @@
 package com.bytelegend;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Challenge {
     public static void main(String[] args) {
         System.out.println(incrementNumbersInCsv("1,2,3"));
@@ -9,11 +12,13 @@ public class Challenge {
     public static String incrementNumbersInCsv(String str) {
         String[] numbers = str.split(",");
         String result = "";
+        int number;
 
         for (int i = 0; i < numbers.length; ++i) {
-            numbers[i] = String.valueOf(Integer.parseInt(numbers[i]) + 1);
+            number = Integer.parseInt(numbers[i]) + 1;
+            numbers[i] = String.valueOf(number);
         }
 
-        return result.join(", " , numbers);
+        return result.join(", ", numbers);
     }
 }
