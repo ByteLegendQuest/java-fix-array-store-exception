60851117bcee43d21779b71081e7dc1ae1792f97
agirnob <kakcayigitbaran@gmail.com>
2022-08-16 18:55:21 +0000

    Update at 2022-08-16T18:55:21.218757Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -7,9 +7,9 @@ public class Challenge {
     }
 
     public static String incrementNumbersInCsv(String str) {
-        Object[] numbers = str.split(",");
-        for (int i = 0; i < numbers.length; ++i) {
-            numbers[i] = Integer.parseInt(numbers[i].toString()) + 1;
+           Object[] numbers = str.split(",");
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i] = Integer.toString(Integer.parseInt((String) numbers[i])+1);
         }
         return String.join(",", (String[]) numbers);
     }
