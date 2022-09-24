fe2608ce22257c1a6c7f59d714b2c51acbd5e675
YuanHao97 <y1997h@126.com>
2022-09-24 06:27:49 +0000

    Update at 2022-09-24T06:27:49.456054Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -8,9 +8,10 @@ public class Challenge {
 
     public static String incrementNumbersInCsv(String str) {
         Object[] numbers = str.split(",");
+        String[] l = new String[numbers.length];
         for (int i = 0; i < numbers.length; ++i) {
-            numbers[i] = Integer.parseInt(numbers[i].toString()) + 1;
+            l[i] = String.valueOf(Integer.parseInt(numbers[i].toString()) + 1);
         }
-        return String.join(",", (String[]) numbers);
+        return String.join(",", l);
     }
 }
