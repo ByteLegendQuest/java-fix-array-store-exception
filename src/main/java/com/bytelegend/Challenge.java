aba1c88fcad457e220c39de4c512d232bdd247a2
keranbingaa <binghong@mail.ustc.edu.cn>
2022-10-16 09:34:06 +0000

    Update at 2022-10-16T09:34:06.074570Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -1,5 +1,7 @@
 package com.bytelegend;
 
+import java.util.Arrays;
+
 public class Challenge {
     public static void main(String[] args) {
         System.out.println(incrementNumbersInCsv("1,2,3"));
@@ -8,9 +10,10 @@ public class Challenge {
 
     public static String incrementNumbersInCsv(String str) {
         Object[] numbers = str.split(",");
+        Object[] nums = new Integer[numbers.length];
         for (int i = 0; i < numbers.length; ++i) {
-            numbers[i] = Integer.parseInt(numbers[i].toString()) + 1;
+            nums[i] = Integer.parseInt(numbers[i].toString()) + 1;
         }
-        return String.join(",", (String[]) numbers);
+        return String.join(",", Arrays.toString(nums));
     }
 }
