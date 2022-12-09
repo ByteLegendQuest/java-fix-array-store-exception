13b682ec27d74669296278fb7b92b95f84bd37eb
jufjzq <136180840@qq.com>
2022-12-09 06:36:45 +0000

    Update at 2022-12-09T06:36:45.634452Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -7,9 +7,9 @@ public class Challenge {
     }
 
     public static String incrementNumbersInCsv(String str) {
-        Object[] numbers = str.split(",");
-        for (int i = 0; i < numbers.length; ++i) {
-            numbers[i] = Integer.parseInt(numbers[i].toString()) + 1;
+        String[] numbers = str.split(",");
+        for (int i = 0; i < numbers.length; i++) {
+        numbers[i] = Integer.toString(Integer.parseInt(numbers[i]) + 1);
         }
         return String.join(",", (String[]) numbers);
     }
