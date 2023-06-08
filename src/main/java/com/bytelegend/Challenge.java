ab851af2b1bb9add9fd62c069d5d6555f0dac1e2
Windsority <mcfengxin@gmail.com>
2023-06-08 03:37:21 +0000

    Update at 2023-06-08T03:37:21.649531Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -9,13 +9,10 @@ public class Challenge {
     public static String incrementNumbersInCsv(String str) {
         Object[] numbers = str.split(",");
         for (int i = 0; i < numbers.length; ++i) {
-            numbers[i] = Integer.parseInt(numbers[i].toString()) + 1;
+            int x = Integer.parseInt(numbers[i].toString()) + 1;
+            numbers[i] = Integer.toString(x);
         }
-        String returnString = "";
-        if (numbers.length > 0) returnString += numbers[0].toString();
-        for (int i = 1; i < numbers.length; ++i) {
-            returnString += "," + numbers[i].toString();
-        }
-        return returnString;
+        return String.join(",", (String[]) numbers);
     }
+}
 }
